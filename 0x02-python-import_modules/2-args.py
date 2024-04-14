472d8fcaf2a64ef12fddc6928953d8cd47be5244
OFHATWA <tofhatwa@gmail.com>
2024-04-14 14:40:13 +0200

    print number of argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == __main__:
+    
+    from sys import argv
+    length = len(argv) - 1
+    
+    if length < 1:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+    else:
+        print("{} arguments:".format(length))
+    for argumnt in range(length):
+        print("{}: {:s}".format(argumnt + 1, argv[argumnt + 1]))
