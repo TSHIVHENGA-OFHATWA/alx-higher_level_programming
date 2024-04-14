6ed630a3cd089318621eb9fea9225599466e5b38
OFHATWA <tofhatwa@gmail.com>
2024-04-14 17:05:18 +0200

    4-hidden_discovery.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "main":
+if __name__ == "__main__":
     from sys import argv
     result = 0
     for loop in range(1, len(argv)):
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    names = dir()
+    for loop in range(0, len(names))
+    if names[loop][:2] != "__":
+        print("{:s}".format(names[loop]))
