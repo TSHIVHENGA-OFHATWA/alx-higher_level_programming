cfdc7389b37fa1fbeb423849a3c739421ef8663c
OFHATWA <tofhatwa@gmail.com>
2024-05-17 16:02:21 +0200

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+
+"""
+The module defines a class Squares.
+
+The module contains:
+    - A sheband line specifying the path to the python interpreter.
+    - A square class with a docstring debribing its purpuse.
+"""
+
+
+class Square:
+    """
+        A class used to represent a Square.
+
+        Args:
+            size (int): The size of the square (optional, default is 0).
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is less than 0.
+        """
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def get_size(self):
+        """Return the size of the square."""
+        return self.__size
+
+    def set_size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
