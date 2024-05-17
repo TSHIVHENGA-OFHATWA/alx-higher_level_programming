b3ab96c64cfd846986180daa8e1a50eec5b4bf99
OFHATWA <tofhatwa@gmail.com>
2024-05-17 06:37:11 +0200

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,15 +7,14 @@ The module defines a class Squares.
 The module contains:
     - A sheband line specifying the path to the python interpreter.
     - A square class with a docstring debribing its purpuse.
-    
 """
+
 
 class Square:
     """
         A class used to represent a Square.
 
         Private attribute representing size.
-                
         """
     def __init__(self, size):
         self.__size = size
