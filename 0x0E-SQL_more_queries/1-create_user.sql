02aed1da0afcddc975a6eeef8ffee9f66b1787db
OFHATWA <tofhatwa@gmail.com>
2024-05-23 00:12:56 +0200

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,5 @@
 -- The user_0d_1 password should be set to user_0d_1_pwd
 DROP USER IF EXISTS "user_0d_1"@"localhost";
 CREATE USER "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
-GRANT SELECT ON `mysql`.* TO "user_0d_1"@"localhost";
+GRANT SELECT 
+ON *.* TO "user_0d_1"@"localhost";
