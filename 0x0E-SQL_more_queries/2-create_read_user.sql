1c84e1baedab3021bbc6830eaded8ae754a9f9bd
OFHATWA <tofhatwa@gmail.com>
2024-05-23 04:27:27 +0200

    create read user sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
+-- user
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbthn_0d_2`. * TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+-- priviledges
+GRANT SELECT ON `hbthn_0d_2`.* TO 'user_0d_2'@'localhost';
