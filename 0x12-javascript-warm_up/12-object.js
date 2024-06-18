bc31bb570413556d888dc4d15b5ba7799c39ce54
OFHATWA <tofhatwa@gmail.com>
2024-06-18 23:00:55 +0200

    13-add.js

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-function updateArgs(args) {
-  const nums = args.map(Number);
-  const index = nums.indexOf(12);
-  if (index > -1) {
-    nums[index] = 89;
-  }
-  return nums;
-}
-const args = process.argv.slice(2);
-console.log(updateArgs(args));
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
