adf84ee7313400df0d4853f763cbe53b2a377219
OFHATWA <tofhatwa@gmail.com>
2024-06-18 21:32:08 +0200

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-
 const args = process.argv.slice(2);
 const num = parseInt(args[0], 10);
-console.log(num === NaN ? 'Not a number' : `My number: ${num}`);
+if (num === NaN) {
+  console.log(`My number: ${num}`);
+}
+else {
+  console.log('Not a number');
+}
