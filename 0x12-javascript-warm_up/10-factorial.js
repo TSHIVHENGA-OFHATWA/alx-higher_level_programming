4c899da4140c961abd7535795a24d6d1a5e72eef
OFHATWA <tofhatwa@gmail.com>
2024-06-18 22:47:59 +0200

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n)) {
     return 1;
   } if (n === 0) {
     return 1;
   }
-return n * factorial(n - 1);
-  }
+  return n * factorial(n - 1);
+}
 const arg = process.argv[2];
 const num = parseInt(arg, 10);
 console.log(factorial(num));
