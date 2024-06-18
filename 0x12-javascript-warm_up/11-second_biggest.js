c85df07f638f60bc2e2cda26466d640cdcc50b36
OFHATWA <tofhatwa@gmail.com>
2024-06-18 22:55:45 +0200

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,10 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const args = process.argv.slice(2).map(Number);
 const secondBiggest = (arr) => {
   if (arr.length <= 1) return 0;
-  let max = -Infinity, secondMax = -Infinity;
+
+  let max = -Infinity; let secondMax = -Infinity;
+
   for (const num of arr) {
     if (num > max) {
       secondMax = max;
@@ -11,6 +13,7 @@ const secondBiggest = (arr) => {
       secondMax = num;
     }
   }
+
   return secondMax;
 };
 console.log(secondBiggest(args));
