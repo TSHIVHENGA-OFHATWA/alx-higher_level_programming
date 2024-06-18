43da2a1faeb3fb0f1ee599373150670b8fb15a58
OFHATWA <tofhatwa@gmail.com>
2024-06-18 22:37:18 +0200

    8-square.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 function add (a, b) {
-    const num1 = parseInt(a);
-    const num2 = parseInt(b);
-    if (isNaN(num1) || isNaN(num2)) {
-        console.log(NaN);
-    } else {
-        console.log(num1 + num2);
-    }
+  const num1 = parseInt(a);
+  const num2 = parseInt(b);
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log(NaN);
+  } else {
+    console.log(num1 + num2);
+  }
 }
 const args = process.argv.slice(2);
 add(args[0], args[1]);
