007f072021d70d652f8b052cd9a23a2b4d2e61f1
OFHATWA <tofhatwa@gmail.com>
2024-06-19 10:10:00 -0700

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,7 @@ class Square extends Square5 {
     for (let row = 0; row < this.height; row++) {
       let toPrint = '';
       for (let column = 0; column < this.width; column++) {
-        toPrint += 'X';
+        toPrint += c;
       }
       console.log(toPrint);
     }
