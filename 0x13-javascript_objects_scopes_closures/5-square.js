e5627b004c6320b014a7c7e049fce00c9a4322a2
OFHATWA <tofhatwa@gmail.com>
2024-06-19 10:53:26 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Rectangles = require('./4-rectangle');
 
-class Square extends Rectangle {
+class Square extends Rectangles {
   constructor (size) {
     super(size, size);
   }
