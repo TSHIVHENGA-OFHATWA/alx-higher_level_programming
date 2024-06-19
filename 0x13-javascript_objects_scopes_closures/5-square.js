6eaa501868fbc2731f9f5c19f45fa5668a584435
OFHATWA <tofhatwa@gmail.com>
2024-06-19 17:20:07 +0200

    5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
