7e992123f8de6946100f674741e871c42dc53c93
ofhatwa <tofhatwa@gmail.com>
2024-07-14 19:23:04 +0200

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that sends a POST request to a URL with an email 
+as a parameter and displays the response body.
+"""
+import urllib.parse
+import urllib.request
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+with urllib.request.urlopen(url, data) as response:
+    print(response.read().decode('utf-8'))
