925d828bc72e1156d077d207f009f691bc597ddd
ofhatwa <tofhatwa@gmail.com>
2024-07-14 21:03:28 +0200

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,11 +9,22 @@ import urllib.request
 import urllib.error
 import sys
 
-url = sys.argv[1]
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
+def fetch_url(url):
+    """
+    Fetches the content of the URL and prints
+    the body of response (decoded in utf-8).
+    If an HTTPError occurs, it prints "Error code:"
+    followed by the HTTP status code.
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    fetch_url(url)
