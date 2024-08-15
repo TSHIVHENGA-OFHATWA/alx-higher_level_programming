0081aca9c70a76128beded1f5eba6985f6bd2cbb
ofhatwa <tofhatwa@gmail.com>
2024-08-15 21:45:44 +0200

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 Function that adds two integers or floats.
 """
 
+
 def add_integer(a, b=98):
     """
     Adds two integers or floats. Floats are cast to integers.
@@ -16,11 +17,16 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: If a or b are not integers or floats.
+        ValueError: If a or b are NaN or infinity.
     """
-    if not (isinstance(a, int) or isinstance(a, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int) or isinstance(b, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    return int(a) + int(b)
 
+    if isinstance(a, float) and (a != a or abs(a) == float('inf')):
+        raise ValueError("cannot convert float NaN or infinity to integer")
+    if isinstance(b, float) and (b != b or abs(b) == float('inf')):
+        raise ValueError("cannot convert float NaN or infinity to integer")
+
+    return int(a) + int(b)
